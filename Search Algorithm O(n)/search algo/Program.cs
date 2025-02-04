7eeb27a3add1283a6af1066ad14e3c6f507d3c3c
Anthony Cassero <casseroant@outlook.com>
2025-02-03 23:36:39 -0500

    Add files via upload

---
+++ Search Algorithm O(n)/search algo/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace search_algo
+{
+    using System;
+
+    public class Program
+    {
+        
+        // time complexity: O(n) due to having to search each variable in the array until the end.
+        // in my search the number 22 is not found, so we do step through the enitre array
+
+        // space complexity: O(1) due to the limited varaibles and the memory is constanlty being used
+
+        public static int lSearch(int[] a, int x)
+        {
+            int array = a.Length;
+            
+
+            int notFound = 0;
+
+            //base case
+            for (int i = 0; i < array; i++) //start at index 0, if not found step through the array until index (x) found
+                                              //this is the worst case, and what will happen in my code
+            {
+
+                if (a [i] == x)
+
+                    return i; //if found
+            }
+
+            return notFound;
+
+            
+        }
+        //loop termination once x is either found, or at the end when its not found
+
+        
+        public static void Main(string[] args)
+        {
+
+            int x = 22;
+
+            int[] numberA = { 0, 34, 66, 2, 91, 44, 80, 45, 72 }; 
+                      
+                
+            
+            int result = lSearch(numberA, x); //displays the result from the search method in result
+
+
+            Console.WriteLine("The array holds: ");
+
+            Console.WriteLine(string.Join(" ", numberA));
+
+            if (result != 0) 
+            {
+                Console.WriteLine($"At index {result}, {x} was found in the array");
+            }
+
+            else
+            {
+                Console.WriteLine($"The number {x} was not found in the array");
+            }
+        }
+    }
+}
---
+++ Search Algorithm O(n)/search algo/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("search algo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("search algo")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d29e4f55-97a4-48de-8889-8ec0f27b328f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
